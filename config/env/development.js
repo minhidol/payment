949e38547c151a785c3b347e7a53f083c6d94d9b
minhnn45 <minhnn1403@gmail.com>
2022-08-09 15:59:03 +0700

    ad

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@ module.exports = {
     env: "local",
     db: "mongodb://localhost:27017/payment",
     ACCESS_TOKEN_SECRET: "DEVELOPMENT_@123abcd",
-    ACCESS_TOKEN_LIFE: "2h",
+    ACCESS_TOKEN_LIFE: "12h",
     jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
     ACCESS_TOKEN_LIFE_MOBILE: "365d",
     token:
