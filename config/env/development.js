78c3a21cbf7261e7fa2b0c8688b9b48264816b30
Nguyen Nhat Minh (FTEL ISC HCM) <minhnn45@fpt.com.vn>
2022-09-15 21:11:08 +0700

    a

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: "local",
-    db: "mongodb://root:root@localhost:2017/payment",
+    db: "mongodb://minhnn:cnvietn@localhost:2017/payment",
     
     ACCESS_TOKEN_SECRET: "DEVELOPMENT_@123abcd",
     ACCESS_TOKEN_LIFE: "5h",
